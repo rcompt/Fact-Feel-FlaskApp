67283836d6e30f9087a61ba103abf635887324a3
Zachary Trahan <zacktrahan15@gmail.com>
2021-03-21 10:32:50 -0700

    Added some properties based on the UML that we'll use to display information - Brought in LiveCharts package - Stubbed TODOs as questions came up - Split data structures into a Client/Server that the main view model knows about

---
+++ FactFeelUI/FactFeelView/Client/ClientViewModel.cs
@@ -0,0 +1,101 @@
+﻿using FactFeelMainView.Utilities;
+using System;
+using LiveCharts;
+using System.Windows.Media;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace FactFeelUI.Client
+{
+    public class ClientViewModel : ObservableBase
+    {
+        private readonly int NumberOfLights;
+
+        public ClientViewModel(int numberOfLights)
+        {
+            //TODO: Figure out if it's possible to query state of script any time
+            //      - CurrentTextSequence
+            //      - IsRecording
+            //      - ErrorState
+
+            NumberOfLights = numberOfLights;
+
+            lightList = new ObservableCollection<Brush>();
+            for (int i = 0; i < numberOfLights; i++)
+            {
+                lightList.Add(Brushes.Transparent);
+            }
+        }
+
+        private SeriesCollection outputData = null; //TODO: rename. The UML calls this "Timeseries of outputs"
+        public SeriesCollection OutputData
+        {
+            get
+            {
+                if (outputData == null)
+                {
+                    outputData = new SeriesCollection();
+                }
+
+                return outputData;
+            }
+        }
+
+        private double sequenceSuccessRate = 0;
+        public double SequenceSuccessRate
+        {
+            get
+            {
+                return sequenceSuccessRate;
+            }
+            set
+            {
+                SetProperty(ref sequenceSuccessRate, value);
+            }
+        }
+
+        ObservableCollection<Brush> lightList = null;
+        ObservableCollection<Brush> LightList
+        {
+            get
+            {
+                return lightList;
+            }
+            set
+            {
+                SetProperty(ref lightList, value);
+            }
+        }
+
+        private string currentRecognizedText = string.Empty;
+        public string CurrentRecognizedText
+        {
+            get
+            {
+                return currentRecognizedText;
+            }
+            set
+            {
+                SetProperty(ref currentRecognizedText, value);
+            }
+        }
+
+        private int currentSequenceID = 0;
+        public int CurrentSequenceID
+        {
+            get
+            {
+                return currentSequenceID;
+            }
+            set
+            {
+                SetProperty(ref currentSequenceID, value);
+            }
+        }
+
+        private double CalculateSuccessRate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FactFeelUI/FactFeelView/Server/ServerViewModel.cs
@@ -0,0 +1,34 @@
+﻿using FactFeelMainView.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactFeelUI.Server
+{
+    public class ServerViewModel : ObservableBase
+    {
+        public ServerViewModel()
+        {
+            //TODO: Figure out if it's possible to query state of script any time
+            //      - IsProcessing
+            //      - IsVectorizing
+            //      - ErrorState
+        }
+
+        //TODO: want this here or just client side?
+        private double currentPrediction = 0;
+        public double CurrentPrediction
+        {
+            get
+            {
+                return currentPrediction;
+            }
+            set
+            {
+                SetProperty(ref currentPrediction, value);
+            }
+        }
+    }
+}
