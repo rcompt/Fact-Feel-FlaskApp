8de5bc0f953d9295d5d1064c8230db76a1a34126
Zachary Trahan <zacktrahan15@gmail.com>
2021-03-22 19:35:00 -0700

    Moved entire C# code to root level of app. Added all non c# files to solution so they can be easily viewed within VS

---
+++ FactFeelView/Client/ClientModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactFeelUI.Client
+{
+    /// <summary>
+    /// Wrapper for client python script
+    /// </summary>
+    public class ClientModel
+    {
+
+
+    }
+}
