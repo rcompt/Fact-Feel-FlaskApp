7162af835daa4e3ba30c62173ee76922d723e16c
Zachary Trahan <zacktrahan15@gmail.com>
2021-03-22 20:33:45 -0700

    Imported python extensions Added some hooks for configuration

---
+++ FactFeelView/Resources/FactFeelConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactFeelUI.Resources
+{
+    public class FactFeelConfig
+    {
+        public string PythonExe { get; private set; }
+        public string FileName { get; private set; }
+
+        public FactFeelConfig()
+        {
+
+        }
+    }
+}
