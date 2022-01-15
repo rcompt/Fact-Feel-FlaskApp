467426b7890a9954aee2bd51766a704b5c726742
Ryan Compton <ryancompton.james@gmail.com>
2022-01-15 13:21:16 -0800

    Updated relative paths

--- Fact-Feel-App/model/Fact_Feel_Regression.py
+++ Fact-Feel-App/model/Fact_Feel_Regression.py
@@ -8,25 +8,24 @@ Created on Fri Dec  6 15:10:10 2019
 import pickle
 import os
 
-#Temporary fix, need to provide main project path
-#TODO: Fix need for absolute paths on importing other scripts
-#os.chdir("C:\\Users\\Ryan\\Documents\\Projects\\Fact-Feel-FlaskApp")
-
-from sklearn import linear_model
-
 from feature_extraction.feature_extractor import FeatureExtractor
 
 class FactFeelRegressor():
     
     def __init__(self):
+        self.model_path = os.path.dirname(os.path.abspath(__file__))
+        self.model_path = os.path.join(
+            self.model_path.split("Fact-Feel-App")[0], 
+            "Fact-Feel-App",
+            "model")
         
-        with open(os.path.join(os.getcwd(),"model","Fact_Feel_normalizer.pkl"),"rb") as f_p:
+        with open(os.path.join(self.model_path,"Fact_Feel_normalizer.pkl"),"rb") as f_p:
             self.normalizer = pickle.load(f_p)
         
         self.model = self.load_model()
         
     def load_model(self):
-        with open(os.path.join(os.getcwd(),"model","Linear_Model_qr_py3.pkl"),"rb") as f_p:
+        with open(os.path.join(self.model_path,"Linear_Model_qr_py3.pkl"),"rb") as f_p:
             return pickle.load(f_p,encoding="latin-1")
         
     def predict(self,data):
